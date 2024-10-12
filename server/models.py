5a98356e021a25b8822be92697cf6fbe2314befa
franklynekirwa2 <sangkirwa2@gmail.com>
2024-10-12 15:35:40 +0300

    update

--- server/models.py
+++ server/models.py
@@ -4,29 +4,56 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
-    __tablename__ = 'customers'
+class Review(db.Model, SerializerMixin):
+    __tablename__ = "reviews"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    comment = db.Column(db.String, nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey("items.id"), nullable=False)
 
-    def __repr__(self):
-        return f'<Customer {self.id}, {self.name}>'
+    # Relationships
+    customer = db.relationship("Customer", back_populates="reviews")
+    item = db.relationship("Item", back_populates="reviews")
+
+    # Serialization rules to prevent circular dependencies
+    serialize_rules = ("-customer.reviews", "-item.reviews")
 
 
-class Item(db.Model):
-    __tablename__ = 'items'
+class Customer(db.Model, SerializerMixin):
+    __tablename__ = "customers"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    price = db.Column(db.Float)
+    name = db.Column(db.String, nullable=False)
 
-    def __repr__(self):
-        return f'<Item {self.id}, {self.name}, {self.price}>'
+    # Relationship to Review
+    reviews = db.relationship("Review", back_populates="customer")
+
+    # Association proxy to get items through reviews
+    items = association_proxy("reviews", "item")
+
+    # Serialization rule
+    serialize_rules = ("-reviews.customer",)
+
+
+class Item(db.Model, SerializerMixin):
+    __tablename__ = "items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    # Relationship to Review
+    reviews = db.relationship("Review", back_populates="item")
+
+    # Serialization rule
+    serialize_rules = ("-reviews.item",)
